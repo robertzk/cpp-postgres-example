d290bc54bd592db0a5681a33f74e86fa2a392ef3
robertzk <technoguyrob@gmail.com>
2016-01-22 19:55:55 -0600

    Example of heterogeneous types.

--- src/app.cpp
+++ src/app.cpp
@@ -6,7 +6,7 @@ namespace cpp_postgres_app {
     std::cout << "Hello world" << "\033[1;31mbold red text\033[0m\n";
     std::cout << "How's " << crayon("it going?", "green") << std::endl;
     for (const auto &el : arguments) {
-      std::cout << std::endl << "\033[31m" << el << "\033[39m !!";
+      std::cout << std::endl << "\033[31m" << el << "\033[39m !!" << std::endl;
     }
 
     db_app();
@@ -22,14 +22,29 @@ namespace cpp_postgres_app {
       SELECT a, b FROM test
     )");
 
-    std::vector<int> as;
-    for (unsigned int i = 0; i < result.size(); i++) {
-      as.push_back(result[i][0].as<int>());
-    }
+    std::string annotation[2] = {"a", "b"};
+    for (unsigned int k = 0; k < 2; k++) {
+      auto type = annotation[k];
+      std::vector<int> i_els;
+      std::vector<std::string> s_els;
+      for (unsigned int i = 0; i < result.size(); i++) {
+        if (k == 0) {
+          i_els.push_back(result[i][k].as<int>());
+        } else {
+          s_els.push_back(result[i][k].as<std::string>());
+        }
+      }
 
-    std::cout << "The a's in the database are: \n\n";
-    for (const auto &el : as) {
-      std::cout << crayon(std::to_string(el), "blue") << std::endl;
+      std::cout << "The " << type << "'s in the database are: \n\n";
+      if (k == 0) {
+        for (const auto &el : i_els) {
+          std::cout << crayon(std::to_string(el), "blue") << std::endl;
+        }
+      } else {
+        for (const auto &el : s_els) {
+          std::cout << crayon(el, "red") << std::endl;
+        }
+      }
     }
 
     transaction.commit();
