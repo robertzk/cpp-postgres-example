c06434a91c4dcfaaa4e377981f2fcc3acf449d68
robertzk <technoguyrob@gmail.com>
2016-01-22 19:48:33 -0600

    Add schema

---
+++ schema.sql
@@ -0,0 +1,41 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: test; Type: TABLE; Schema: public; Owner: robertk; Tablespace: 
+--
+
+CREATE TABLE test (
+    a integer,
+    b character varying(32)
+);
+
+
